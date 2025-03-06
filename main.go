260e3e9f994476128952d51f25822c79c63ac712
ahmed <ahmed@baid.com>
2025-03-06 11:07:14 +0000

    first commit in lem-in

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import parsing_test "lem-in/parsing"
+
+func main() {
+	parsing_test.Parse()
+}
---
+++ utils/tools.go
@@ -0,0 +1,15 @@
+package utils
+
+func Tools() {
+	type Rooms struct {
+		Name string
+		X, Y string
+
+	}
+	type Khaliya struct{
+		Number_ants int
+		Start *Rooms
+		End *Rooms
+		Link map[string][]string
+	}
+}
