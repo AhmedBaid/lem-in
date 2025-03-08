18e4feeaab1667641d3d012a33a04957ce916eb3
Your Name <you@example.com>
2025-03-08 13:14:08 +0000

    add communt

--- parsing/parsfile.go
+++ parsing/parsfile.go
@@ -10,26 +10,28 @@ import (
 )
 
 func Parsing() *utils.AntFarm {
+
+	// Create a new AntFarm struct
 	colony := &utils.AntFarm{
 		Start: &utils.Room{},
 		End:   &utils.Room{},
 		Rooms: make(map[string]*utils.Room),
 		Links: make(map[string][]string),
 	}
-
+// check if the number of arguments is equal to 2
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: ./lem-in [filename]")
-		os.Exit(1)
+		return nil
 
 	}
-
+// read the file
 	file, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return nil
 
 	}
-
+// split the file into lines
 	line := strings.Split(string(file), "\n")
 	nbrAnts, err := strconv.Atoi(line[0])
 	if err != nil {
@@ -37,13 +39,13 @@ func Parsing() *utils.AntFarm {
 		return nil
 
 	}
-
+// check if the number of ants is greater than 0
 	if nbrAnts <= 0 {
 		fmt.Println("Error: Number of ants must be greater than 0")
 		return nil
 
 	}
-
+// assign the number of ants to the colony
 	colony.NumAnts = nbrAnts
 
 	StartDup := false
@@ -54,26 +56,28 @@ func Parsing() *utils.AntFarm {
 		if line[i] == "" {
 			continue
 		}
-
+// create a new room
 		room := &utils.Room{
 			Name: "",
 			X:    "",
 			Y:    "",
 		}
 		rooms := strings.Fields(line[i])
-
+// check if the number of rooms is equal to 3
 		if len(rooms) == 3 {
+			//  check if starts with 'L' or '#'
 			if rooms[0][0] == 'L' || rooms[0][0] == '#' {
 				fmt.Println("Error: Room name cannot start with 'L' or '#'")
 				return nil
 			}
 
+			// check if the room name is already in the map
 			if _, exists := colony.Rooms[rooms[0]]; exists {
 
 				fmt.Println("ERROR: invalid data format (duplicate room name)")
 				return nil
 			}
-
+// check if the coordinates are duplicates
 			for _, v := range colony.Rooms {
 				if v.X == rooms[1] && v.Y == rooms[2] {
 					fmt.Println("ERROR: invalid data format (duplicate coordinates)")
@@ -81,7 +85,7 @@ func Parsing() *utils.AntFarm {
 
 				}
 			}
-
+// assign the room name, x and y coordinates
 			room.Name = rooms[0]
 			room.X = rooms[1]
 			room.Y = rooms[2]
@@ -89,13 +93,16 @@ func Parsing() *utils.AntFarm {
 			colony.Rooms[room.Name] = room
 
 		}
-
+		
+// check if the line starts with '##start'
 		if strings.TrimSpace(line[i-1]) == "##start" {
+			// check if the start is duplicated
 			if StartDup {
 				fmt.Println("ERROR: invalid data format (start or end  is depleted)")
 				return nil
 			}
 			StartDup = true
+			// add the start room to the colony 
 			if len(rooms) == 3 {
 
 				colony.Start.Name = rooms[0]
@@ -105,14 +112,15 @@ func Parsing() *utils.AntFarm {
 
 			continue
 		}
-
+// check if the line starts with '##end'
 		if strings.TrimSpace(line[i-1]) == "##end" {
+			// check if the end is duplicated
 			if EndDup {
 				fmt.Println("ERROR: invalid data format (start or end  is depleted)")
 				return nil
 			}
 			EndDup = true
-
+// add the end room to the colony
 			if len(rooms) == 3 {
 
 				colony.End.Name = rooms[0]
@@ -123,7 +131,7 @@ func Parsing() *utils.AntFarm {
 		}
 
 		link := strings.Split(line[i], "-")
-
+//add the links to the colony
 		if len(link) == 2 {
 
 			colony.Links[link[0]] = append(colony.Links[link[0]], link[1])
@@ -134,12 +142,12 @@ func Parsing() *utils.AntFarm {
 		}
 
 	}
-
+// check if the start, end or rooms are missing
 	if colony.Start.Name == "" || colony.End.Name == "" || len(colony.Rooms) == 0 {
 		fmt.Println("ERROR: invalid data format (missing start, end or rooms)")
 		return nil
 	}
-
+// check if the links are missing (if have a link of unkonwn room)
 	for  v := range colony.Links {
 		if _, exists := colony.Rooms[v]; !exists {
 			fmt.Println("ERROR: invalid data format (missing links)")
